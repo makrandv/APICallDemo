da682913f2a618febe925a3ef2d7e020d171fbe4
Makrand Velhal <makrand83@gmail.com>
2019-01-09 08:31:16 +1300

    Added Step Definition File

--- APICallDemo/ReadMe.md
+++ APICallDemo/ReadMe.md
@@ -20,32 +20,24 @@ The test case is written in Business Driver Development(BDD) format with the acc
 Project Folder Structure :
 
 - APIClientFiles 
-			|
-			+--> APIClient.cs : This files contains implementation of the methods used to call requested API and read the response from the API.
-			|
-			+--> RootObject.cs : This file contains class definition for the serialized object created after converting the JSON response of the API to C# object.
+  a. APIClient.cs : This files contains implementation of the methods used to call requested API and read the response from the API.
+  b.RootObject.cs : This file contains class definition for the serialized object created after converting the JSON response of the API to C# object.
 			
 - FeatureFiles
-			|
-			+--> APIResponse.Feature : This file contains test scenario in the gherkin syntax.
+  a.APIResponse.Feature : This file contains test scenario in the gherkin syntax.
 
 - StepDefinitions
-			|
-			+--> APIResponseStepDefinitions.cs : This file implements the gherkin statements added to the feature file to call the verification methods
+  a.APIResponseStepDefinitions.cs : This file implements the gherkin statements added to the feature file to call the verification methods
 
 - ValidationLogic
-			|
-			+--> VerifyAPIResponse.cs :  This file implements verification methods which compares the 
-									     expected values of the parameters with actual values of the parameters in the response.
+  a. VerifyAPIResponse.cs : This file implements verification methods which compares the expected values of the parameters with actual values of the parameters in the response.
 
 - packages.config : This file lists are the nuget packages added to project
 
 - App.Config : This file defines configuration added to the project (e.g. reference to Specflow configuration)
 
 
-Test Exeuction : 
-
-1. From Visual Studio :
+Test Execution : 
 	a. Open the solution file "APICallDemo.sln" in Visual Studio.
 	b. Build the solution from Visual Studio
 	c. Click "Test >> Windows" menu and select "Test Explorer" menu item
---
+++ APICallDemo/StepDefinitions/APIResponseStepDefinitions.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TechTalk.SpecFlow;
+
+namespace APICallDemo.StepDefinitions
+{
+    /// <summary>
+    /// This binding class defines the implementation to each of the gherkin style statements used in the feature file
+    /// </summary>
+    [Binding]
+    public class APIResponseStepDefinitions
+    {
+        RootObject deserializedProduct;
+
+        /// <summary>
+        /// Method which calls the API URL and read the response
+        /// </summary>
+        /// <param name="pApiurl">API URL to be called</param>
+        [When(@"I make call to Product API using the URL ""(.*)""")]
+        public void WhenIMakeCallToProductAPIUsingTheURL(string pApiurl)
+        {
+            string output = "";
+            output = APIClient.ReturnResponse(pApiurl).Result;
+
+            //Serializing the Json output of the API call to a RootObject class instance
+            deserializedProduct = JsonConvert.DeserializeObject<RootObject>(output);
+        }
+
+        /// <summary>
+        /// Verify if the element\parameter in the response has the expected value 
+        /// </summary>
+        /// <param name="parameterToBeChecked">Element\Parameter in the response to be verified</param>
+        /// <param name="compareCondition">Compare condition (equals  or contains)</param>
+        /// <param name="expectedParameterValue">Expected value of the parameter</param>
+        [When(@"I verify response has parameter ""(.*)"" with the value ""(.*)"" to ""(.*)""")]
+        public void ThenIVerifyResponseHasParameterWithTheValueTo(string parameterToBeChecked, string compareCondition, string expectedParameterValue)
+        {
+            //Get the actual parameter value in the response from the deserialzed response object
+            //Used GetType and GetProperty method to make implementation generic irrespective of the parameter passed for verification.
+            string actualElementValue = deserializedProduct.GetType().GetProperty(parameterToBeChecked).GetValue(deserializedProduct).ToString();
+
+            //Comparing actual value of the response with expected value
+            Assert.IsTrue(VerifyAPIResponse.VerifyParameterValue(expectedParameterValue, compareCondition, actualElementValue));
+        }
+
+        /// <summary>
+        /// Verify if the given list object in the response has the elements with expected values
+        /// </summary>
+        /// <param name="listObject">List Object to be verified</param>
+        /// <param name="parameterList">Parameters in the list object to be verified</param>
+        /// <param name="compareConditions">Compare condition (equals  or contains)</param>
+        /// <param name="expectedParameterValues">Expected parameter values</param>
+        [When(@"I verify response has '(.*)' listitem with one item having '(.*)' with '(.*)' and values '(.*)'")]
+        public void WhenIVerifyResponseHasListitemWithOneItemHavingWithAndValues(string listObject, string parameterList, string compareConditions, string expectedParameterValues)
+        {
+            //Get the list object in the response from the deserialzed response object
+            object elementListObj = deserializedProduct.GetType().GetProperty(listObject).GetValue(deserializedProduct);
+
+            //Converting list object into a generic object
+            List<object> result = ((IEnumerable)elementListObj).Cast<object>().ToList();
+
+            //Comparing actual value of the required parameters in the response with their expected values
+            Assert.IsTrue(VerifyAPIResponse.VerifyParameterListValue(listObject, result.ToArray(), parameterList, compareConditions, expectedParameterValues));
+        }
+
+    }
+
+}
